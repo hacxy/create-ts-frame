9a76260196e3c0a2866c6c2348d68000e393c8b6
Hacxy <hacxy.97@outlook.com>
2024-04-28 12:58:49 +0800

    fix(template): update template

--- templates/cli-tsup/src/index.ts
+++ templates/cli-tsup/src/index.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const bootstrap = async () => {
-  console.log('🥳 Welcome Welcome to use the CLI Tsup Template!');
+  console.log('🥳 Welcome to use the CLI Tsup Template!');
 };
 
 bootstrap();
